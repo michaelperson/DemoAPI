75af4847c2e72f69975420d53b9ab2dd1543e356
Person M <michael.person@cognitic.be>
2021-12-10 15:04:10 +0100

    Configuration du Boostrapper pour l'app wpf mvvm

---
+++ WpfSignalR/Bootstrapper.cs
@@ -0,0 +1,27 @@
+﻿using Prism.Ioc;
+using Prism.Unity;
+using System.Windows;
+
+namespace WpfSignalR
+{
+    public class Bootstrapper : PrismBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<Window>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+             
+        }
+
+        protected override void InitializeShell(DependencyObject shell)
+        {
+            base.InitializeShell(shell); 
+
+            Application.Current.MainWindow = (Window)this.Shell;
+            Application.Current.MainWindow.Show();
+        }
+    }
+}
